c0be86ad49b8013d5280fb7008186a66c7a0c742
Владислав <vladislav1999.19.08@gmail.com>
2023-02-07 17:36:44 +0300

    search

--- main.py
+++ main.py
@@ -1,1 +1,52 @@
 import pygame
+
+
+def main():
+    screen = pygame.display.set_mode((640, 480))
+
+    font = pygame.font.Font(None, 32)
+    title = pygame.display.set_caption('Search')
+    clock = pygame.time.Clock()
+    input_box = pygame.Rect(200, 200, 140, 32)
+    color_inactive = pygame.Color('lightskyblue3')
+    color_active = pygame.Color('dodgerblue2')
+    color = color_inactive
+    active = False
+    text = ''
+    done = False
+
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if input_box.collidepoint(event.pos):
+                    active = not active
+                else:
+                    active = False
+                color = color_active if active else color_inactive
+            if event.type == pygame.KEYDOWN:
+                if active:
+                    if event.key == pygame.K_RETURN:
+                        print(text)
+                        text = ''
+                    elif event.key == pygame.K_BACKSPACE:
+                        text = text[:-1]
+                    else:
+                        text += event.unicode
+
+        screen.fill((30, 30, 30))
+        txt_surface = font.render(text, True, color)
+        width = max(200, txt_surface.get_width() + 10)
+        input_box.w = width
+        screen.blit(txt_surface, (input_box.x + 5, input_box.y + 5))
+        pygame.draw.rect(screen, color, input_box, 2)
+
+        pygame.display.flip()
+        clock.tick(30)
+
+
+if __name__ == '__main__':
+    pygame.init()
+    main()
+    pygame.quit()
